5b09851da0e8e2a87247fffcde94b76a74622230
Travis Luong <travis@travisluong.com>
2022-07-10 13:14:22 -0700

    initial commit

---
+++ src/main/java/com/example/fullstackbooktodospringboot/FullstackbookTodoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackbooktodospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackbookTodoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackbookTodoSpringbootApplication.class, args);
+    }
+
+}
