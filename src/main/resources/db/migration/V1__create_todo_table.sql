dc9c15980ff5f8898908a56f43e6f05cfbc030dc
travisluong <travis@travisluong.com>
2022-08-04 13:22:56 -0700

    replace liquibase with flyway

--- src/main/resources/db/migration/V1__create_todo_table.sql
+++ src/main/resources/db/migration/V1__create_todo_table.sql
@@ -1,6 +1,3 @@
---liquibase formatted sql
-
---changeset fullstackbook:1
 create table todos (
   id bigserial primary key,
   name text
--- src/main/resources/db/migration/V2__add_completed_to_todo.sql
+++ src/main/resources/db/migration/V2__add_completed_to_todo.sql
@@ -1,4 +1,1 @@
---liquibase formatted sql
-
---changeset fullstackbook:2
 alter table todos add column completed boolean not null default false;
